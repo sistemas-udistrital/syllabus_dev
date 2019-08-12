26daa835501699162faa8f49fd13a344f001b9bb
apdaza <apdaza@gmail.com>
2019-08-11 20:04:59 -0500

    agregados agregar syllabus y docente

--- app/app.py
+++ app/app.py
@@ -55,6 +55,55 @@ def syllabus():
     rows = cur.fetchall();
     return render_template('list_syllabus.html',rows = rows, title = title, titles = titulos)
 
+@app.route('/add_syllabus', methods = ['POST', 'GET'])
+def add_syllabus():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        codigo = request.form['codigo']
+        proyecto = int(request.form['proyecto'])
+        docente = int(request.form['docente'])
+
+        conn = mysql.connect()
+        cur = conn.cursor()
+
+        cur.execute("INSERT INTO espacio_academico (esa_nombre,esa_codigo,prc_id,doc_id) VALUES (%s,%s,%s,%s)",(nombre,codigo,proyecto,docente) )
+        conn.commit()
+
+        msg = "Record successfully added"
+        title = "Agregar syllabus"
+        return render_template("result.html",msg = msg, title = title)
+    else:
+        conn = mysql.connect()
+        cur = conn.cursor()
+        campos = "prc_id, prc_nombre, prc_codigo";
+        cur.execute("select " + campos + " from proyecto_curricular")
+        title = "Agregar syllabus"
+        rows_proyectos = cur.fetchall();
+        campos = "doc_id, doc_nombre, doc_apellido";
+        cur.execute("select " + campos + " from docente")
+        rows_docentes = cur.fetchall();
+        return render_template('add_syllabus.html',proyectos = rows_proyectos, docentes = rows_docentes, title = title)
+
+@app.route('/add_docente', methods = ['POST', 'GET'])
+def add_docente():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        email = request.form['email']
+        documento = request.form['documento']
+
+        conn = mysql.connect()
+        cur = conn.cursor()
+
+        cur.execute("INSERT INTO docente (doc_nombre,doc_apellido,doc_email,doc_documento) VALUES (%s,%s,%s,%s)",(nombre,apellido,email,documento) )
+        conn.commit()
+
+        msg = "Record successfully added"
+        title = "Agregar docente"
+        return render_template("result.html",msg = msg, title = title)
+    else:
+        title = "Agregar docente"
+        return render_template('add_docente.html', title = title)
 
 if __name__ == '__main__':
     app.run(debug = True)
